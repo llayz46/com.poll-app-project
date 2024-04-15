7d4e1c0e90a16d3826ec43666381ce606ab3e104
llayz46 <layzer@outlook.fr>
2024-04-15 16:57:23 +0200

    adding config.php file to .gitignore

---
+++ lib/config_template.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'YOUR DB NAME');
+define('DB_USER', 'YOUR DB USER');
+define('DB_PASSWORD', 'YOUR DB PASSWORD');
+
+define('PATH_ASSETS_IMAGES', '/assets/images/');
+
+define('HOME_LIMIT_POLLS', 3);
+
+define('HEADER_TITLE', ' - Pol layZ');
